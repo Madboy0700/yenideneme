45af9973d258c5a2786fe3f7ce707e51a3120ddc
Madboy0700 <bobimobi07@gmail.com>
2024-12-17 18:00:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("21269322"))
+API_ID = int(getenv("API_ID", "21269322"))
+
 API_HASH = getenv("6a00b295480c9b9ceff34b43a4b92977")
 
 BOT_TOKEN = getenv("7656669753:AAHC7nwjYsfzL0d-z2rFCgai5X1hv-JdJis")
